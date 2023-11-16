15ee58fb6f5ca2381b388b8b832932f8a2019f40
Vipulpandey007 <vipulpandey708@gmail.com>
2023-11-16 20:26:17 +0530

    updated footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="Footer-container">
+      <h1>Made By Vipul</h1>
+    </div>
+  );
+};
+
+export default Footer;
--- src/myjs.js
+++ src/myjs.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 //inline css
 
 const Applayout = () => {
@@ -9,6 +10,7 @@ const Applayout = () => {
     <div className="app">
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 };
