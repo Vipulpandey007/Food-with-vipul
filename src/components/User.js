38d1911716acde6cca35a45fb29f2e2bf4add5c6
Vipulpandey007 <vipulpandey708@gmail.com>
2024-02-11 20:18:08 +0530

    image updated

--- src/components/User.js
+++ src/components/User.js
@@ -22,7 +22,10 @@ class User extends React.Component {
   render() {
     return (
       <div className="user-card">
-        <img src={this.state.userinfo.avatar_url} />
+        <img
+          className="rounded-full w-32"
+          src="https://avatars.githubusercontent.com/u/123075222?s=400&u=7b4b9efd3e49dbd48bf8f99e60b54e496603c08d&v=4"
+        />
         <h2>Name:- {this.state.userinfo.login}</h2>
         <h3>Location:-Ranchi,Jharkhand</h3>
         <h3>Contact:- 8340311564</h3>
